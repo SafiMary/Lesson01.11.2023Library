5897844f51e9286b774bd0e0d79886539880ca78
С - 7 < - 7@WIN-1B59ROQRL87>
2023-11-08 20:15:24 +0500

    Add project files.

---
+++ Lesson01.11.2023Library/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Lesson01._11._2023Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьИзмененияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.DGAutor = new System.Windows.Forms.DataGridView();
+            this.DGBooks = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AutorName = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGAutor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.правкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // выбратьToolStripMenuItem
+            // 
+            this.выбратьToolStripMenuItem.Name = "выбратьToolStripMenuItem";
+            this.выбратьToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.выбратьToolStripMenuItem.Text = "выбрать БД";
+            this.выбратьToolStripMenuItem.Click += new System.EventHandler(this.выбратьToolStripMenuItem_Click);
+            // 
+            // правкаToolStripMenuItem
+            // 
+            this.правкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateToolStripMenuItem,
+            this.сохранитьИзмененияToolStripMenuItem});
+            this.правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            this.правкаToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // CreateToolStripMenuItem
+            // 
+            this.CreateToolStripMenuItem.Name = "CreateToolStripMenuItem";
+            this.CreateToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.CreateToolStripMenuItem.Text = "создать таблицы";
+            this.CreateToolStripMenuItem.Click += new System.EventHandler(this.CreateToolStripMenuItem_Click);
+            // 
+            // сохранитьИзмененияToolStripMenuItem
+            // 
+            this.сохранитьИзмененияToolStripMenuItem.Name = "сохранитьИзмененияToolStripMenuItem";
+            this.сохранитьИзмененияToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.сохранитьИзмененияToolStripMenuItem.Text = "сохранить изменения";
+            this.сохранитьИзмененияToolStripMenuItem.Click += new System.EventHandler(this.сохранитьИзмененияToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.CheckFileExists = false;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // DGAutor
+            // 
+            this.DGAutor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGAutor.Location = new System.Drawing.Point(12, 61);
+            this.DGAutor.Name = "DGAutor";
+            this.DGAutor.Size = new System.Drawing.Size(240, 377);
+            this.DGAutor.TabIndex = 1;
+            this.DGAutor.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGAutor_CellDoubleClick);
+            // 
+            // DGBooks
+            // 
+            this.DGBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGBooks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.AutorName});
+            this.DGBooks.Location = new System.Drawing.Point(333, 61);
+            this.DGBooks.Name = "DGBooks";
+            this.DGBooks.Size = new System.Drawing.Size(290, 377);
+            this.DGBooks.TabIndex = 1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "ID";
+            this.dataColumn1.DataType = typeof(int);
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "Name";
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "Номер";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // AutorName
+            // 
+            this.AutorName.DataPropertyName = "id_autor";
+            this.AutorName.DataSource = this.dataSet1;
+            this.AutorName.DisplayMember = "Table1.Name";
+            this.AutorName.HeaderText = "ФИО";
+            this.AutorName.Name = "AutorName";
+            this.AutorName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.AutorName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.AutorName.ValueMember = "Table1.ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DGBooks);
+            this.Controls.Add(this.DGAutor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGAutor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DGBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem выбратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem правкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CreateToolStripMenuItem;
+        private System.Windows.Forms.DataGridView DGAutor;
+        private System.Windows.Forms.DataGridView DGBooks;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьИзмененияToolStripMenuItem;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewComboBoxColumn AutorName;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+    }
+}
+
---
+++ Lesson01.11.2023Library/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+
+namespace Lesson01._11._2023Library
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection conn;
+        DataTable dtAutor;
+        DataTable dtBooks;
+        SQLiteDataAdapter adAutor;
+        SQLiteDataAdapter adBooks;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void выбратьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filename;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                conn = new SQLiteConnection("DataSource=" + filename);
+                conn.Open();
+                string sqltext = "select name from sqlite_master where type='table'";
+                using (SQLiteCommand cmd = new SQLiteCommand(sqltext, conn))
+                {
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+                    CreateToolStripMenuItem.Enabled = !reader.HasRows;
+                    if (reader.HasRows)
+                    {
+                        FillAutor();
+                        FillBooks();
+                    }
+                    reader.Close();
+                    conn.Close();
+                }
+
+            }
+        }
+
+        private void CreateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string sqltext = "create table Autors (id int primary key, name varchar(20),descripton varchar(20));" +
+                "create table Books (id int primary key, id_autor int, name varchar(20),descripton varchar(20));";
+            using(SQLiteCommand cmd= new SQLiteCommand(sqltext,conn)){
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            CreateToolStripMenuItem.Enabled=false;
+
+        }
+        private void FillAutor()
+        {
+            string sqltextA = "select id,name,descripton from autors";
+            adAutor = new SQLiteDataAdapter(sqltextA, conn);
+            adAutor.SelectCommand = new SQLiteCommand(sqltextA, conn);
+            SQLiteCommandBuilder cb = new SQLiteCommandBuilder(adAutor);
+            dtAutor = new DataTable();
+            adAutor.Fill(dtAutor);
+            DGAutor.DataSource = dtAutor;
+            dtAutor.Columns[0].AutoIncrement= true;
+            dtAutor.Columns[0].AutoIncrementStep= 1;
+            dtAutor.Columns[0].AutoIncrementSeed = 10;
+
+
+        }
+        private void FillBooks(string author_id="")
+        {
+            string sqltextB = "select books.id,books.id_autor," +
+                " autors.name, books.name, books.descripton from books"+
+                " join autors on autors.id=books.id_autor";
+            if (!string.IsNullOrEmpty(author_id))
+                sqltextB = sqltextB + " where id_autor=" + author_id;
+            dtBooks = new DataTable();
+            adBooks = new SQLiteDataAdapter(sqltextB, conn);
+            adBooks.SelectCommand =new SQLiteCommand(sqltextB,conn);
+            sqltextB = "insert into books values (@id,@id_autor,@name,@descripton);";
+            adBooks.InsertCommand = new SQLiteCommand(sqltextB,conn);
+            adBooks.InsertCommand.Parameters.Add("id", DbType.Int32, 5, "id");
+            adBooks.InsertCommand.Parameters.Add("id_autor", DbType.Int32, 5, "id_autor");
+            adBooks.InsertCommand.Parameters.Add("name", DbType.String, 20, "name");
+            adBooks.InsertCommand.Parameters.Add("descripton", DbType.String, 20, "descripton");
+            sqltextB = "update books set id_autor=@id_autor,name=@name,descripton=@descripton where id=@id";
+            adBooks.UpdateCommand = new SQLiteCommand(sqltextB,conn);
+            adBooks.UpdateCommand.Parameters.Add("id", DbType.Int32, 5, "id");
+            adBooks.UpdateCommand.Parameters.Add("id_autor", DbType.Int32, 5, "id_autor");
+            adBooks.UpdateCommand.Parameters.Add("name", DbType.String, 20, "name");
+            adBooks.UpdateCommand.Parameters.Add("descripton", DbType.String, 20, "descripton");
+            sqltextB = "delete from books where id=@id";
+            adBooks.DeleteCommand= new SQLiteCommand(sqltextB,conn);
+            adBooks.DeleteCommand.Parameters.Add("id", DbType.Int32, 5, "id");
+
+            adBooks.Fill(dtBooks);
+            DGBooks.DataSource = dtBooks;
+
+            dtBooks.Columns[0].AutoIncrement = true;
+            dtBooks.Columns[0].AutoIncrementStep = 1;
+            dtBooks.Columns[0].AutoIncrementSeed = 10;
+            adAutor.Fill(dataSet1);
+            MessageBox.Show(DGBooks.Columns[1].Name);
+            (DGBooks.Columns["AutorName"] as DataGridViewComboBoxColumn).DataSource = dtAutor;
+            (DGBooks.Columns["AutorName"] as DataGridViewComboBoxColumn).DataPropertyName= "id_autor";
+            (DGBooks.Columns["AutorName"] as DataGridViewComboBoxColumn).ValueMember= "ID";
+            (DGBooks.Columns["AutorName"] as DataGridViewComboBoxColumn).DisplayMember= "Name";
+        }
+
+        private void сохранитьИзмененияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            adAutor.Update(dtAutor);
+            adBooks.Update(dtBooks);
+        }
+
+        private void DGAutor_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            FillBooks(DGAutor[0, DGAutor.CurrentRow.Index].Value.ToString());
+
+        }
+    }
+}
